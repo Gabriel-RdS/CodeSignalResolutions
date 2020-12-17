9fb974d2d9ee338e22acb4624d3d822d6646cf51
Gabriel Ramos dos Santos <gabrielramosds@outlook.com>
2020-12-17 11:43:59 -0300

    Adicionando mais duas opções de resolução do desáfio rotelmage.py

--- rotatelmage.py
+++ rotatelmage.py
@@ -8,3 +8,18 @@ import numpy as np
 def rotateImage(array):
     return np.rot90(array, k=3, axes=(0, 1))
 
+# or
+
+
+def rotateImage2(a):
+    a.reverse()
+    for i in range(len(a)):
+        for j in range(i):
+            a[i][j], a[j][i] = a[j][i], a[i][j]
+    return a
+
+# or
+
+
+def rotateImage3(a):
+    return [[x[i] for x in a][::-1] for i in range(len(a))]
